4eca97199aa53c4898668e42136484dd21734301
mani <mani95lisa@gmail.com>
2015-03-31 00:00:51 +0800

    u

--- index.js
+++ index.js
@@ -19,6 +19,4 @@ app = module.exports = express();
 options = require('./lib/spec')(app)
 app.use(kraken(options));
 app.on('start', function () {
-    console.log('Application ready to serve requests.');
-    console.log('Environment: %s', app.kraken.get('env:env'));
 });
